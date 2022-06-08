0486bf1d7b96a9fb0fd74474d7c03180ab108a5d
erpozo <pozo.erp.a@gmail.com>
2022-06-08 11:41:07 +0200

    Insercion de datos

---
+++ DBdata.sql
@@ -0,0 +1,33 @@
+INSERT INTO `itec`.`titulacion`
+    (`Nombre`)
+    VALUES (`Grado Medio`);
+
+INSERT INTO `itec`.`titulacion`
+    (`Nombre`)
+    VALUES (`Grado Superior`);
+
+INSERT INTO `itec`.`titulacion`
+    (`Nombre`)
+    VALUES (`Idiomas`);
+
+INSERT INTO `itec`.`curso`
+(`curso_ID`,
+`Nombre`,
+`fk_ciclo_ID`)
+VALUES
+(<{curso_ID: }>,
+<{Nombre: }>,
+<{fk_ciclo_ID: }>);
+
+
+INSERT INTO `itec`.`ciclo`
+    (`Nombre`)
+    VALUES (`Desarrollo Aplicaciones Web`);
+
+INSERT INTO `itec`.`ciclo`
+    (`Nombre`)
+    VALUES (`Desarrollo Aplicaciones Multiplataforma`);
+
+INSERT INTO `itec`.`ciclo`
+    (`Nombre`)
+    VALUES (``);
